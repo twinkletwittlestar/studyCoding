25a5e48812b7b469b82e9071980d785785f8aeb4
Scarlett O'Hara <46996053+twinkletwittlestar@users.noreply.github.com>
2020-01-03 19:26:19 +0900

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,67 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos'; //toDos를 그대로 쓰기에는 String이라서 변수로써 사용하기 위함인 듯?
+
+let toDos = [];
+
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo){
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+  const li = document.createElement("li");  //요소도 "" 적어도 이 내장함수에서는.
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  delBtn.addEventListener("click", deleteToDo);
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos()
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos(){
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null){
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo){
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function init(){
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
